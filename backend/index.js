40dec1a231089fd6960411e0bcccd616fa523287
APSALJAMAL <jamalapsal@gmail.com>
2024-09-27 17:52:53 +0530

    Create sign up API route

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
-import userRouter from './routes/user.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 const app=express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, () => {
 	connectDB();
@@ -13,3 +16,4 @@ app.listen(PORT, () => {
 });
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
